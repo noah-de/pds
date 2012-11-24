c46322c92d7c9ae1126e113cbd2b97f9cab184b4
Noah <noah.de@gmail.com>
2012-11-23 20:18:07 -0800

    - Added a test drive of the Array ADT.

---
+++ arrayImplemented.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+arrayImplemented.py
+
+Created by versorge on 2012-11-23.
+"""
+
+from array import Array
+
+# Create an array for the counters and initialize each element to 0.
+theCounters = Array(127)
+theCounters.clear(0)
+
+# Open the text file for reading and counting
+theFile = open('Frankenstein.txt','r')
+for line in theFile:
+	for letter in line:
+		code = ord(letter)
+		theCounters[code] += 1
+theFile.close()
+
+# Print the results
+for i in range(26):
+	print ("%c - %4d                %c - %4d" % (chr(65+i), theCounters[65+i], chr(97+i), theCounters[97+i]))
+
